cac044ecb53c3e83f1927bc936101a16a9c7dd11
Erik <erikjtsai@gmail.com>
2024-01-10 18:46:25 -0500

    Updated changes

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [datetime, setDatetime] = useState('');
+  const [description, setDescription] = useState('');
+  const [transactions, setTransactions] = useState('');
+ 
+
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions(){
+    const url = process.env.REACT_APP_API_URL+'/transactions';
+    const response = await fetch(url);
+    return await response.json();
+  }
+
+
+
+
+  function addNewTransaction(ev){
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split(' ')[0];
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-type':'application/json'},
+      body: JSON.stringify({
+        name:name.substring(price.length + 1), 
+        price,
+        description, 
+        datetime
+      })
+    }).then(res => {
+      res.json().then(data => {
+        setName('');
+        setDatetime('');
+        setDescription('');
+        getTransactions().then(setTransactions);
+        console.log('result', data);
+      });
+    });
+
+  }
+  
+  
+  
+  
+   
+  let balance = 0;
+  for(const transaction of transactions){
+    balance += transaction.price;
+  }
+  
+  balance = balance.toFixed(2);
+  const fraction =  balance.split('.')[1];
+  balance =  balance.split('.')[0];
+
+
+  return (
+    <main>
+      <h1>${balance}<span>{fraction}</span></h1>
+      <form onSubmit={addNewTransaction}>
+        <div className='basic'>
+          <input type='text' value={name} onChange={ev => setName(ev.target.value)} placeholder='+200 Transaction Name ' />
+          <input type='datetime-local' value={datetime} onChange={ev => setDatetime(ev.target.value)}/>
+        </div>
+        <div className='description'>
+          <input type='text' value={description} onChange={ev => setDescription(ev.target.value)} placeholder='Transaction Description' />
+        </div>
+        <button type='submit'>Add</button>
+      </form>
+      
+      <div className='transactions'>
+        {transactions.length > 0 && transactions.map(transaction => (
+          <div className='transaction'>
+          <div className='left'>
+            <div className='name'>{transaction.name}</div>
+            <div className='description'>{transaction.description}</div>
+          </div>
+          <div className='right'>
+            <div className={"price " + (transaction.price < 0?'red':'green')}>
+              {transaction.price}
+            </div>
+            <div className='datetime'>{transaction.datetime}</div>
+          </div>
+        </div>
+        ))}
+        
+      </div>
+    </main>
+  );
+}
+
+export default App;
